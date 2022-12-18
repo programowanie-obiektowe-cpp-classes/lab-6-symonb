153326f5c9f1c3d78b2fe4c573959da221353271
Szymon <symonbielenin@gmail.com>
2022-12-18 20:36:32 +0100

    1st

--- foo.hpp
+++ foo.hpp
@@ -1,7 +1,8 @@
 #pragma once
-
 #include "Human.hpp"
+#include <iostream>
 
+#include "make_random_vector.hpp"
 #include <algorithm>
 #include <list>
 #include <vector>
@@ -9,8 +10,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-
-    // Twoja implementacja tutaj
+    int                 counter = 0;
+    std::for_each(people.begin(), people.end(), [&](Human& per) {
+        per.birthday();
+        retval[retval.size() - counter - 1] = per.isMonster() ? 'n' : 'y';
+        counter++;
+    });
 
     return retval;
 }
